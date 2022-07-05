43c7cd0e91405aa16853f20939a610308ae21d3e
Kaminsky <kaminskyfrontend@gmail.com>
2022-07-05 18:21:58 +0300

    hygen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import { Button } from "./components/Button/Button";
+
 
 function App() {
   return (
     <header className="App-header">
       <p>Hello React</p>
+      <Button text={'Click Me'} />
     </header>
   );
 }
