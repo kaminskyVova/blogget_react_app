75a60474774abb627e2596b4f993ac80bd6b4279
Kaminsky <kaminskyfrontend@gmail.com>
2022-07-16 05:00:13 +0300

    reddit api auth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,23 +1,32 @@
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 
 export const useAuth = (urlApi, setAuth, token, delToken) => {
 	useEffect(() => {
 		if (!token) return;
 
-		fetch(`${urlApi}/api/v1/me`, {
-			headers: {
-				Authorization: `bearer ${token}`,
-			},
-		})
-			.then((response) => response.json())
-			.then(({ name, icon_img: iconImg }) => {
-				const img = iconImg.replace(/\?.*$/, '');
-				setAuth({ name, img });
-			})
-			.catch((err) => {
-				console.err(err);
-				delToken();
-				setAuth({});
+		try {
+			fetch(`${urlApi}/api/v1/me`, {
+				headers: {
+					Authorization: `bearer ${token}`,
+				},
+			}).then((response) => {
+				if (response.status === 401) {
+					console.log('Ошибка статус 401');
+					delToken();
+					setAuth({});
+				} else if (response.status === 200) {
+					response.json().then(({ name, icon_img: iconImg }) => {
+						if (name) {
+							const img = iconImg.replace(/\?.*$/, '');
+							setAuth({ name, img });
+						}
+					});
+				}
 			});
+		} catch (err) {
+			console.err(err);
+			delToken();
+			setAuth({});
+		}
 	}, [urlApi, setAuth, token, delToken]);
 };
